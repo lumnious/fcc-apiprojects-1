56ed052638a63312f914d75fa62083b43344244b
lumnious <dotico@gmail.com>
2016-05-31 11:35:20 +0100

    Removed not needed files. Updated start page according to example

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +34,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+/*
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -45,6 +44,7 @@ if (app.get('env') === 'development') {
     });
   });
 }
+*/
 
 // production error handler
 // no stacktraces leaked to user
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var timestamp = require('../public/javascripts/timestamp.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Timestamp microservice' });
+});
+
+router.get('/*/*', function(req, res, next) {
+  res.send('adeus');
+});
+
+router.get('/*', function(req, res, next) {
+  res.send('olá');
 });
 
 module.exports = router;
